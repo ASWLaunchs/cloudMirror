cbfd3a9d3f5331037441f4c1f2fbacc484060d39
ASWLaunchs <1732747279@qq.com>
2022-02-18 03:37:19 +0800

    added poster generation function.

--- controllers/controllers_default/videos.go
+++ controllers/controllers_default/videos.go
@@ -3,6 +3,8 @@ package controllers_default
 import (
 	"cloudMirror/models/hlsConverter"
 	"net/http"
+	"path"
+	"strings"
 	"text/template"
 )
 
@@ -11,10 +13,19 @@ type ControllerVideos struct{}
 func (c ControllerVideos) Videos(w http.ResponseWriter, r *http.Request) {
 	ffmpegPath := "E:/env/pkg/ffmpeg-master-latest-win64-gpl-shared/bin/ffmpeg.exe"
 	srcPath := "static/assets/videos/example.mp4"
-	targetPath := "static/assets/videos"
+	targetPath := "static/assets/hls"
+	framePosition := "1"
 	targetFilename := "example.m3u8"
-	resOptions := []string{"480p"}
-	hlsConverter.HlsConverter("video", ffmpegPath, srcPath, targetPath, targetFilename, resOptions)
+	resOptions := []string{"480p", "720p"}
+
+	//create final targetPath
+	str := strings.Split(srcPath, "/")
+	targetPath = path.Join(targetPath, str[len(str)-1])
+
+	//call HLS converter.
+	hlsConverter.Init(targetPath)
+	hlsConverter.GeneratePoster(ffmpegPath, srcPath, targetPath, framePosition)
+	hlsConverter.HlsConverter(ffmpegPath, srcPath, targetPath, targetFilename, resOptions)
 
 	t, _ := template.ParseFiles("templates/templates_default/video.html")
 	t.Execute(w, r)
--- models/hlsConverter/hlsConverter.go
+++ models/hlsConverter/hlsConverter.go
@@ -6,6 +6,40 @@ import (
 	"os/exec"
 )
 
+//Init() -> initialize file storage path.
+//if you want to use hlsConverter make something , then you must use Init() before your any step.
+func Init(targetPath string) {
+	exist, err := PathExists(targetPath)
+	if err != nil {
+		fmt.Printf("get dir error![%v]\n", err)
+		return
+	}
+	if exist {
+		fmt.Printf("has dir![%v]\n", targetPath)
+	} else {
+		fmt.Printf("no dir![%v]\n", targetPath)
+		// create dir.
+		err := os.Mkdir(targetPath, os.ModePerm)
+		if err != nil {
+			fmt.Printf("mkdir failed![%v]\n", err)
+		} else {
+			fmt.Printf("mkdir success!\n")
+		}
+	}
+}
+
+//PathExists() judege the dir exist or not.
+func PathExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
+
 // GenerateHLS() be used to generate the hls files by resolutions.
 // The available resolutions are: 360p, 480p, 720p and 1080p..
 func GenerateHLS(ffmpegPath, srcPath, targetPath, resolution string) error {
@@ -28,19 +62,26 @@ func GenerateHLSCustom(ffmpegPath string, options []string) error {
 	return err
 }
 
-//HlsConverter main execution function.
-func HlsConverter(category string, ffmpegPath string, srcPath string, targetPath string, targetFilename string, resOptions []string) {
-	if category == "post" {
+//GeneratePoster() generate poster according to source video file.
+func GeneratePoster(ffmpegPath string, srcPath string, targetPath string, framePosition string) error {
+	cmd := exec.Command(ffmpegPath, "-i", srcPath, "-ss", framePosition, "-frames:v", "1", targetPath+"/poster.jpg")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
 
-	} else if category == "video" {
-		variants, _ := GenerateHLSVariant(resOptions, "")
-		GeneratePlaylist(variants, targetPath, targetFilename)
+	err := cmd.Start()
+	return err
+}
 
-		for _, res := range resOptions {
-			err := GenerateHLS(ffmpegPath, srcPath, targetPath, res)
-			if err != nil {
-				fmt.Println(err)
-			}
+//HlsConverter() main execution function.
+//generate m3u8 file and ts file.
+func HlsConverter(ffmpegPath string, srcPath string, targetPath string, targetFilename string, resOptions []string) {
+	variants, _ := GenerateHLSVariant(resOptions, "")
+	GeneratePlaylist(variants, targetPath, targetFilename)
+
+	for _, res := range resOptions {
+		err := GenerateHLS(ffmpegPath, srcPath, targetPath, res)
+		if err != nil {
+			fmt.Println(err)
 		}
 	}
 }
